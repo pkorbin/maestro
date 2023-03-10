d291acd82b4a4408f8f78410b4ac728b7d2a097b
pkorbin <55298456+pkorbin@users.noreply.github.com>
2023-03-10 23:56:01 +0100

    Update robin.js

--- robin.js
+++ robin.js
@@ -1,24 +1,33 @@
-function convertDivsToTables() {
-  const divs = document.querySelectorAll("#page-container > div");
-  divs.forEach((div) => {
-    const table = document.createElement("table");
-    table.className = div.className;
-    table.id = div.id;
-    div.parentNode.replaceChild(table, div);
-    const tbody = document.createElement("tbody");
-    table.appendChild(tbody);
-    const childDivs = div.querySelectorAll("div");
-    childDivs.forEach((childDiv) => {
-      const tr = document.createElement("tr");
-      tbody.appendChild(tr);
+function convertDivsToTable() {
+  // Skapa en ny <table>-element
+  const table = document.createElement("table");
+  table.classList.add("table");
+  // Hitta <div>-elementet med id "page-container"
+  const pageContainer = document.querySelector("#page-container");
+  // Loopa igenom alla <div>-element som är barn till "page-container"
+  pageContainer.querySelectorAll("div").forEach(div => {
+    // Skapa ett nytt <tr>-element
+    const tr = document.createElement("tr");
+    // Loopa igenom alla <div>-element som är barn till det nuvarande <div>-elementet
+    div.querySelectorAll("div").forEach(subDiv => {
+      // Skapa ett nytt <td>-element
       const td = document.createElement("td");
-      td.className = childDiv.className;
-      td.id = childDiv.id;
-      tr.appendChild(td);
-      while (childDiv.firstChild) {
-        td.appendChild(childDiv.removeChild(childDiv.firstChild));
+      // Kopiera över alla attribut från <div>-elementet till <td>-elementet
+      for (let i = 0; i < subDiv.attributes.length; i++) {
+        const attr = subDiv.attributes[i];
+        td.setAttribute(attr.name, attr.value);
       }
+      // Flytta över alla barn-element från <div>-elementet till <td>-elementet
+      while (subDiv.firstChild) {
+        td.appendChild(subDiv.firstChild);
+      }
+      // Lägg till <td>-elementet som ett barn till <tr>-elementet
+      tr.appendChild(td);
     });
+    // Lägg till <tr>-elementet som ett barn till <tbody>-elementet i tabellen
+    table.appendChild(tr);
   });
+  // Ersätt <div id="page-container"> med den nya <table>-elementet i DOM:en
+  pageContainer.parentNode.replaceChild(table, pageContainer);
 }
 convertDivsToTables('#page-container')
